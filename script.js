12fbf0c6911b27441607e73b51cb329887663c8f
ajm673 <ajm673@mail.umsl.edu>
2021-03-29 18:28:52 -0500

    Changed the JS so bricks previously clicked on go blank

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+var lastBrick = ""
+
 function brickClicked(brick)
 {
 	document.getElementById(brick).innerHTML = brick;
+	if(lastBrick != ""){
+		document.getElementById(lastBrick).innerHTML = "";
+	}
+
+	lastBrick = brick;
+	console.log(lastBrick);
 }
