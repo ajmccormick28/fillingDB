fbab503ce2f44b282bf52494cea07325611c592c
walk306 <73974249+walk306@users.noreply.github.com>
2021-04-05 18:54:56 -0500

    added php search db file

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ function brickClicked(brick)
 		document.getElementById(lastBrick).innerHTML = "";
 	}
 
+	var xmlhttp = new XMLHttpRequest();
+
+	// This Receiving Data back from PHP
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(brick).innerHTML = this.responseText;
+		}
+	};
+
+	
 	lastBrick = brick;
 	console.log(lastBrick);
+	
+
+	//Sending data to the PHP file
+	//let variables = "input=" + str + "&filter=" + document.getElementById("searchFilter").value;
+	//console.log(variables);
+	xmlhttp.open("GET", "searchDB.php?" + variables, true);
+	xmlhttp.send();
 }
