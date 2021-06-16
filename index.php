4dcbe69a91ae8c9d7f31da2f86b097fca8ddd2ba
walk306 <73974249+walk306@users.noreply.github.com>
2021-06-16 17:06:34 -0500

    New new new new and improved the forty second

--- index.php
+++ index.php
@@ -43,20 +43,8 @@
     <?php
       include "insertDB.php";
     ?>
-    <div class="parent">
-			<!-- Row 1 -->
-			<div class="brickStyle brickR1C1" id="a001" onclick="brickClicked('a001')"></div>
-			<div class="brickStyle brickR1C2" id="a002" onclick="brickClicked('a002')"></div>
-			<div class="brickStyle brickR1C3" id="a003" onclick="brickClicked('a003')"></div>
-			<!-- Row 2 -->
-			<div class="brickStyle brickR2C1" id="b001" onclick="brickClicked('b001')"></div>
-			<div class="brickStyle brickR2C2" id="b002" onclick="brickClicked('b002')"></div>
-			<div class="brickStyle brickR2C3" id="b003" onclick="brickClicked('b003')"></div>
-			<!-- Row 3 -->
-			<div class="brickStyle brickR3C1" id="c001" onclick="brickClicked('c001')"></div>
-			<div class="brickStyle brickR3C2" id="c002" onclick="brickClicked('c002')"></div>
-			<div class="brickStyle brickR3C3" id="c003" onclick="brickClicked('c003')"></div>
-	   </div>
+		<iframe src="whatever.html" id="bf">
+        </iframe>
 	   <br/>
      
 
